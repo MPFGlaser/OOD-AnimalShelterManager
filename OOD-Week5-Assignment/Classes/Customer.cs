05da6aa294c0cbac085228c04293ba68a3f38df5
Max Glaser <mpf.glaser@gmail.com>
2020-10-01 18:09:36 +0200

    Added fields, constructor, and properties to Customer class.

--- OOD-Week5-Assignment/Classes/Customer.cs
+++ OOD-Week5-Assignment/Classes/Customer.cs
@@ -8,5 +8,22 @@ namespace OOD_Week5_Assignment.Classes
 {
     public class Customer 
     {
+        private string name;
+        private string address;
+        private string zipcode;
+        private string city;
+
+        public Customer(string name, string address, string zipcode, string city)
+        {
+            this.name = name;
+            this.address = address;
+            this.zipcode = zipcode;
+            this.city = city;
+        }
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Zipcode { get; set; }
+        public string City { get; set; }
     }
 }
