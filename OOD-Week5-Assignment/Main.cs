7b0acacb1469dffc66bd126c03b15a52a2a10ded
Max Glaser <mpf.glaser@gmail.com>
2020-10-01 16:43:27 +0200

    Added temporary buttons to test forms.

--- OOD-Week5-Assignment/Main.cs
+++ OOD-Week5-Assignment/Main.cs
@@ -12,9 +12,24 @@ namespace OOD_Week5_Assignment
 {
     public partial class Main : Form
     {
+        private NewAnimal newAnimal;
+        private NewCustomer newCustomer;
+
         public Main()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            newAnimal = new NewAnimal();
+            newAnimal.Show(this);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            newCustomer = new NewCustomer();
+            newCustomer.Show(this);
         }
     }
 }
