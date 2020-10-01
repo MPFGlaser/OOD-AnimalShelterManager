17979cf4a53ab65a5079be6cd95494e8fd51a111
Max Glaser <mpf.glaser@gmail.com>
2020-10-01 17:08:35 +0200

    Added fields and properties to Animal class

--- OOD-Week5-Assignment/Classes/Animal.cs
+++ OOD-Week5-Assignment/Classes/Animal.cs
@@ -8,5 +8,23 @@ namespace OOD_Week5_Assignment.Classes
 {
     public class Animal
     {
+        private int id;
+        private string name;
+        private int age;
+        private gender gender;
+        private bool adopted;
+        private string notes;
+
+        Animal()
+        {
+
+        }
+
+        public int Id { get; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public gender Gender { get; set; }
+        public bool Adopted { get; set; }
+        public string Notes { get; set; }
     }
 }
