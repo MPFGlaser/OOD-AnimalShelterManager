a58e6b3c31a52599ea1e5d5a1cfb1d4127871ee5
Max Glaser <mpf.glaser@gmail.com>
2020-10-01 17:18:48 +0200

    Added constructor and methods to ShelterManager

--- OOD-Week5-Assignment/Classes/ShelterManager.cs
+++ OOD-Week5-Assignment/Classes/ShelterManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OOD_Week5_Assignment.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,34 @@ namespace OOD_Week5_Assignment.Classes
 {
     public class ShelterManager
     {
+        public ShelterManager()
+        {
+
+        }
+
+        public void SaveData()
+        {
+
+        }
+
+        public void LoadData()
+        {
+
+        }
+
+        public void AddDog(string name, int age, Gender gender, bool adopted, string notes)
+        {
+
+        }
+
+        public void AddCat(string name, int age, Gender gender, bool adopted, string notes)
+        {
+
+        }
+
+        public void AddBird(string name, int age, Gender gender, bool adopted, string notes, Size size)
+        {
+
+        }
     }
 }
